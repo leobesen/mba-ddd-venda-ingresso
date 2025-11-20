32e32d8b3e9d9ec92d5ff4f0f249d1503c648b38
Leonardo Besen <leobesen@hotmail.com>
2025-11-20 20:42:58 -0300

    test: add assertions for CustomerId instance and Customer equality

--- src/@core/events/domain/entities/__tests__/customer.entity.spec.ts
+++ src/@core/events/domain/entities/__tests__/customer.entity.spec.ts
@@ -1,14 +1,22 @@
-import { Customer } from "../customer.entity";
+import { Customer, CustomerId } from '../customer.entity';
 
-describe("Customer", () => {
-    test("Should create a client", () => {
-        const customer = Customer.create({
-            name: "John Doe",
-            cpf: "502.908.050-31",
-        });
-        expect(customer).toBeInstanceOf(Customer);
-        expect(customer.id).toBeDefined();
-        expect(customer.name).toBe("John Doe");
-        expect(customer.cpf.value).toBe("50290805031");
+describe('Customer', () => {
+  test('Should create a client', () => {
+    const customer = Customer.create({
+      name: 'John Doe',
+      cpf: '502.908.050-31',
     });
+    expect(customer).toBeInstanceOf(Customer);
+    expect(customer.id).toBeDefined();
+    expect(customer.id).toBeInstanceOf(CustomerId);
+    expect(customer.name).toBe('John Doe');
+    expect(customer.cpf.value).toBe('50290805031');
+
+    const customer2 = new Customer({
+      id: new CustomerId(customer.id.value),
+      name: 'John Doe',
+      cpf: '502.908.050-31',
+    });
+    expect(customer.equals(customer2)).toBe(true);
+  });
 });
