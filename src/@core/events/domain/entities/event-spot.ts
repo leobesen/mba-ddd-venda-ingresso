0678b3d50a849d106a002cef15c8a7e5efcbc7d8
Leonardo Besen <leobesen@hotmail.com>
2026-02-24 06:06:30 -0300

    feat: implement EventSpot entity with constructor and JSON serialization

---
+++ src/@core/events/domain/entities/event-spot.ts
@@ -0,0 +1,44 @@
+import { Entity } from 'src/@core/common/domain/entity';
+import Uuid from 'src/@core/common/domain/value-objects/uuid.vo';
+
+export class EventSpotId extends Uuid {}
+
+export type EventSpotConstructorProps = {
+  id?: EventSpotId | string;
+  location: string | null;
+  is_reserved: boolean;
+  is_published: boolean;
+};
+
+export class EventSpot extends Entity {
+  id: EventSpotId;
+  location: string | null;
+  is_reserved: boolean;
+  is_published: boolean;
+
+  constructor(props: EventSpotConstructorProps) {
+    super();
+    this.id =
+      props.id instanceof EventSpotId ? props.id : new EventSpotId(props.id);
+    this.location = props.location;
+    this.is_reserved = props.is_reserved;
+    this.is_published = props.is_published;
+  }
+
+  static create() {
+    return new EventSpot({
+      location: null,
+      is_reserved: false,
+      is_published: false,
+    });
+  }
+
+  toJSON() {
+    return {
+      id: this.id.value,
+      location: this.location,
+      is_reserved: this.is_reserved,
+      is_published: this.is_published,
+    };
+  }
+}
