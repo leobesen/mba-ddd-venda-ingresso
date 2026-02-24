7cdc65a05aafc46e230ae56eff1141f50068d5c2
Leonardo Besen <leobesen@hotmail.com>
2026-02-23 22:32:52 -0300

    test: remove redundant customer equality check in Customer tests

--- src/@core/events/domain/entities/__tests__/customer.entity.spec.ts
+++ src/@core/events/domain/entities/__tests__/customer.entity.spec.ts
@@ -11,12 +11,5 @@ describe('Customer', () => {
     expect(customer.id).toBeInstanceOf(CustomerId);
     expect(customer.name).toBe('John Doe');
     expect(customer.cpf.value).toBe('50290805031');
-
-    const customer2 = new Customer({
-      id: new CustomerId(customer.id.value),
-      name: 'John Doe',
-      cpf: '502.908.050-31',
-    });
-    expect(customer.equals(customer2)).toBe(true);
   });
 });
