3743a2ae0344f76a1908764ad184eca264417600
Leonardo Besen <leobesen@hotmail.com>
2026-03-11 08:21:27 +0000

    refactor: simplify async methods in PartnerMysqlRepository

--- src/@core/events/infra/db/repositories/partner-mysql.repository.ts
+++ src/@core/events/infra/db/repositories/partner-mysql.repository.ts
@@ -7,18 +7,25 @@ import { IPartnerRepository } from 'src/@core/events/domain/repositories/partner
 export class PartnerMysqlRepository implements IPartnerRepository {
   constructor(private entityManager: EntityManager) {}
 
-  async add(entity: Partner): Promise<void> {
+  add(entity: Partner): Promise<void> {
     this.entityManager.persist(entity);
+
+    return Promise.resolve();
   }
+
   findById(id: string | PartnerId): Promise<Partner | null> {
-    return this.entityManager.findOneOrFail(Partner, {
+    return this.entityManager.findOne(Partner, {
       id: typeof id === 'string' ? new PartnerId(id) : id,
     });
   }
+
   findAll(): Promise<Partner[]> {
     return this.entityManager.find(Partner, {});
   }
-  async delete(entity: Partner): Promise<void> {
+
+  delete(entity: Partner): Promise<void> {
     this.entityManager.remove(entity);
+
+    return Promise.resolve();
   }
 }
