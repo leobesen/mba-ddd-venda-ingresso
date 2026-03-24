e809b7e283a75a1adc1c7e60ef72b6842db9a7a1
Leonardo Besen <leobesen@hotmail.com>
2026-03-24 23:48:53 +0000

    feat: implement IUnitOfWork interface and UnitOfWorkMikroOrm class for transaction management

---
+++ src/@core/common/infra/unit-of-work-mikro-orm.ts
@@ -0,0 +1,15 @@
+import { EntityManager } from '@mikro-orm/mysql';
+import { IUnitOfWork } from '../application/unit-of-work.interface';
+
+export class UnitOfWorkMikroOrm implements IUnitOfWork {
+  constructor(private em: EntityManager) {}
+  commit(): Promise<void> {
+    return this.em.flush();
+  }
+
+  async rollback(): Promise<void> {
+    await new Promise(() => {
+      this.em.clear();
+    });
+  }
+}
