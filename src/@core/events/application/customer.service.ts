cf2083073413b95b537058eb4fca3ef7115903a3
Leonardo Besen <leobesen@hotmail.com>
2026-03-24 23:52:30 +0000

    feat: enhance CustomerService to include transaction management with IUnitOfWork

--- src/@core/events/application/customer.service.ts
+++ src/@core/events/application/customer.service.ts
@@ -1,8 +1,12 @@
+import { IUnitOfWork } from 'src/@core/common/application/unit-of-work.interface';
 import { Customer } from '../domain/entities/customer.entity';
 import { ICustomerRepository } from '../domain/repositories/customer-repository.interface';
 
 export class CustomerService {
-  constructor(private customerRepository: ICustomerRepository) {}
+  constructor(
+    private customerRepository: ICustomerRepository,
+    private uow: IUnitOfWork,
+  ) {}
 
   list() {
     return this.customerRepository.findAll();
@@ -11,6 +15,7 @@ export class CustomerService {
   async register(input: { name: string; cpf: string }) {
     const customer = Customer.create(input);
     await this.customerRepository.add(customer);
+    await this.uow.commit();
     return customer;
   }
 }
