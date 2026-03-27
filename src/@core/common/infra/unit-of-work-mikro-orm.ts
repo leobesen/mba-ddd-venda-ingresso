a2b5d96bdef4240afcaeb0d901f385549ee2d352
Leonardo Besen <leobesen@hotmail.com>
2026-03-27 08:53:41 +0000

    feat: implement transaction management methods in UnitOfWorkMikroOrm

--- src/@core/common/infra/unit-of-work-mikro-orm.ts
+++ src/@core/common/infra/unit-of-work-mikro-orm.ts
@@ -3,6 +3,18 @@ import { IUnitOfWork } from '../application/unit-of-work.interface';
 
 export class UnitOfWorkMikroOrm implements IUnitOfWork {
   constructor(private em: EntityManager) {}
+  runTransaction<T>(fn: () => Promise<T>): Promise<T> {
+    return this.em.transactional(fn);
+  }
+  beginTransaction(): Promise<void> {
+    return this.em.begin();
+  }
+  completeTransaction(): Promise<void> {
+    return this.em.commit();
+  }
+  rollbackTransaction(): Promise<void> {
+    return this.em.rollback();
+  }
   commit(): Promise<void> {
     return this.em.flush();
   }
