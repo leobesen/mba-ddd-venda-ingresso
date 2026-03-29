c2fa0389c652d1bcb29137e2cb4db0579ea5c3b9
Leonardo Besen <leobesen@hotmail.com>
2026-03-29 23:36:02 +0000

    feat: create DatabaseModule for MikroORM integration with MySQL

---
+++ src/database/database.module.ts
@@ -0,0 +1,53 @@
+import { EntityManager, MySqlDriver } from '@mikro-orm/mysql';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Global, Module } from '@nestjs/common';
+import { UnitOfWorkMikroOrm } from 'src/@core/common/infra/unit-of-work-mikro-orm';
+import {
+  CustomerSchema,
+  EventSchema,
+  EventSectionSchema,
+  EventSpotSchema,
+  OrderSchema,
+  PartnerSchema,
+  SpotReservationSchema,
+} from 'src/@core/events/infra/db/schemas';
+
+@Global()
+@Module({
+  imports: [
+    MikroOrmModule.forRoot({
+      driver: MySqlDriver,
+      entities: [
+        CustomerSchema,
+        PartnerSchema,
+        EventSchema,
+        EventSectionSchema,
+        EventSpotSchema,
+        OrderSchema,
+        SpotReservationSchema,
+      ],
+      dbName: 'venda_ingresso',
+      host: 'localhost',
+      port: 3306,
+      user: 'user',
+      password: 'password',
+      forceEntityConstructor: true,
+      driverOptions: {
+        connection: {
+          timezone: 'Z',
+        },
+      },
+    }),
+  ],
+  providers: [
+    {
+      provide: 'IUnitOfWork',
+      useFactory: (em: EntityManager) => {
+        return new UnitOfWorkMikroOrm(em);
+      },
+      inject: [EntityManager],
+    },
+  ],
+  exports: ['IUnitOfWork'],
+})
+export class DatabaseModule {}
