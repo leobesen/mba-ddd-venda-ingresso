035974a682fb242c50d0e7c3361e3c18f3c17788
Leonardo Besen <leobesen@hotmail.com>
2026-03-31 08:50:58 +0000

    refactor: replace unit of work with application service for transaction management in PartnerService

--- src/@core/events/application/partner.service.ts
+++ src/@core/events/application/partner.service.ts
@@ -1,11 +1,11 @@
-import { IUnitOfWork } from 'src/@core/common/application/unit-of-work.interface';
 import { Partner } from '../domain/entities/partner.entity';
 import { IPartnerRepository } from '../domain/repositories/partner-repository.interface';
+import { ApplicationService } from 'src/@core/common/application/application.service';
 
 export class PartnerService {
   constructor(
     private partnerRepository: IPartnerRepository,
-    private uow: IUnitOfWork,
+    private applicationService: ApplicationService,
   ) {}
 
   list() {
@@ -13,22 +13,24 @@ export class PartnerService {
   }
 
   async create(input: { name: string }) {
-    const partner = Partner.create(input);
-    await this.partnerRepository.add(partner);
-    await this.uow.commit();
-    return partner;
+    return await this.applicationService.run(async () => {
+      const partner = Partner.create(input);
+      await this.partnerRepository.add(partner);
+      return partner;
+    });
   }
 
   async update(id: string, input: { name?: string }) {
-    const partner = await this.partnerRepository.findById(id);
-    if (!partner) {
-      throw new Error('Partner not found');
-    }
-    if (input.name) {
-      partner.changeName(input.name);
-    }
-    await this.partnerRepository.add(partner);
-    await this.uow.commit();
-    return partner;
+    return await this.applicationService.run(async () => {
+      const partner = await this.partnerRepository.findById(id);
+      if (!partner) {
+        throw new Error('Partner not found');
+      }
+      if (input.name) {
+        partner.changeName(input.name);
+      }
+      await this.partnerRepository.add(partner);
+      return partner;
+    });
   }
 }
