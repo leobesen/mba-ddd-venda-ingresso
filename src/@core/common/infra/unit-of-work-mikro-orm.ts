7d9420e3a2ae7c6fd089616c17b597057a9d8cf2
Leonardo Besen <leobesen@hotmail.com>
2026-03-31 08:50:13 +0000

    feat: add getAggregateRoots method to IUnitOfWork and UnitOfWorkMikroOrm

--- src/@core/common/infra/unit-of-work-mikro-orm.ts
+++ src/@core/common/infra/unit-of-work-mikro-orm.ts
@@ -1,5 +1,6 @@
 import { EntityManager } from '@mikro-orm/mysql';
 import { IUnitOfWork } from '../application/unit-of-work.interface';
+import { AggregateRoot } from '../domain/aggregate-root';
 
 export class UnitOfWorkMikroOrm implements IUnitOfWork {
   constructor(private em: EntityManager) {}
@@ -23,5 +24,12 @@ export class UnitOfWorkMikroOrm implements IUnitOfWork {
     await new Promise(() => {
       this.em.clear();
     });
+  }
+
+  getAggregateRoots(): AggregateRoot[] {
+    return [
+      ...this.em.getUnitOfWork().getPersistStack(),
+      ...this.em.getUnitOfWork().getRemoveStack(),
+    ] as AggregateRoot[];
   }
 }
