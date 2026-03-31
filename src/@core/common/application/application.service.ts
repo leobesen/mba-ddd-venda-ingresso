9ed0757c72420feb6ac16fb8ab8eb83fdd86593a
Leonardo Besen <leobesen@hotmail.com>
2026-03-31 08:50:23 +0000

    feat: implement ApplicationService class for managing application logic

---
+++ src/@core/common/application/application.service.ts
@@ -0,0 +1,33 @@
+import { DomainEventManager } from '../domain/domain-event-manager';
+import { IUnitOfWork } from './unit-of-work.interface';
+
+export class ApplicationService {
+  constructor(
+    private uow: IUnitOfWork,
+    private domainEventManager: DomainEventManager,
+  ) {}
+
+  start() {}
+
+  async finish() {
+    const aggregateRoots = this.uow.getAggregateRoots();
+    for (const aggregateRoot of aggregateRoots) {
+      await this.domainEventManager.publish(aggregateRoot);
+    }
+    await this.uow.commit();
+  }
+
+  fail() {}
+
+  async run<T>(fn: () => Promise<T>): Promise<T> {
+    this.start();
+    try {
+      const result = await fn();
+      await this.finish();
+      return result;
+    } catch (error) {
+      this.fail();
+      throw error;
+    }
+  }
+}
