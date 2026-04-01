608333edb3d3e001c3805ec77fa492bc77c88722
Leonardo Besen <leobesen@hotmail.com>
2026-04-01 08:39:16 +0000

    feat: create IDomainEventHandler interface and update DomainEventManager to use it

--- src/@core/common/domain/domain-event-manager.ts
+++ src/@core/common/domain/domain-event-manager.ts
@@ -1,5 +1,8 @@
 import EventEmitter2, { ListenerFn } from 'eventemitter2';
 import { AggregateRoot } from './aggregate-root';
+import { IDomainEvent } from './domain-event';
+
+type DomainEventHandler = (event: IDomainEvent) => void | Promise<void>;
 
 export class DomainEventManager {
   eventEmitter: EventEmitter2;
@@ -8,13 +11,14 @@ export class DomainEventManager {
     this.eventEmitter = new EventEmitter2({ wildcard: true });
   }
 
-  register(event: string, handler: ListenerFn) {
-    this.eventEmitter.on(event, handler);
+  register(event: string, handler: DomainEventHandler) {
+    this.eventEmitter.on(event, handler as ListenerFn);
   }
 
   async publish(aggregateRoot: AggregateRoot) {
     for (const event of aggregateRoot.events) {
       await this.eventEmitter.emitAsync(event.constructor.name, event);
     }
+    aggregateRoot.clearEvents();
   }
 }
