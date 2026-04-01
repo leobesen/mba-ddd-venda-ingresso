895676aaf9bbde6d750ce2a2ab00e2830434d824
Leonardo Besen <leobesen@hotmail.com>
2026-04-01 16:48:45 +0000

    refactor: streamline DomainEventManager by separating domain and integration event handling

--- apps/mba-ddd-venda-ingresso/src/@core/common/domain/domain-event-manager.ts
+++ apps/mba-ddd-venda-ingresso/src/@core/common/domain/domain-event-manager.ts
@@ -1,24 +1,38 @@
 import { EventEmitter2, ListenerFn } from 'eventemitter2';
 import { AggregateRoot } from './aggregate-root';
-import { IDomainEvent } from './domain-event';
-
-type DomainEventHandler = (event: IDomainEvent) => void | Promise<void>;
 
 export class DomainEventManager {
-  eventEmitter: EventEmitter2;
+  domainEventsSubscriber: EventEmitter2;
+  integrationEventsSubscriber: EventEmitter2;
 
   constructor() {
-    this.eventEmitter = new EventEmitter2({ wildcard: true });
+    this.domainEventsSubscriber = new EventEmitter2({
+      wildcard: true,
+    });
+    this.integrationEventsSubscriber = new EventEmitter2({
+      wildcard: true,
+    });
   }
 
-  register(event: string, handler: DomainEventHandler) {
-    this.eventEmitter.on(event, handler as ListenerFn);
+  register(event: string, handler: any) {
+    this.domainEventsSubscriber.on(event, handler as ListenerFn);
+  }
+
+  registerForIntegrationEvent(event: string, handler: any) {
+    this.integrationEventsSubscriber.on(event, handler as ListenerFn);
   }
 
   async publish(aggregateRoot: AggregateRoot) {
     for (const event of aggregateRoot.events) {
-      await this.eventEmitter.emitAsync(event.constructor.name, event);
+      const eventClassName = event.constructor.name;
+      await this.domainEventsSubscriber.emitAsync(eventClassName, event);
     }
-    aggregateRoot.clearEvents();
+  }
+
+  async publishForIntegrationEvent(aggregateRoot: AggregateRoot) {
+    for (const event of aggregateRoot.events) {
+      const eventClassName = event.constructor.name;
+      await this.integrationEventsSubscriber.emitAsync(eventClassName, event);
+    }
   }
 }
