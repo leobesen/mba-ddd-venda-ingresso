c85db83437bbb2cbfd2c3b0a616be5e2d7bf5c55
Leonardo Besen <leobesen@hotmail.com>
2026-04-01 16:37:20 +0000

    feat: add IntegrationEventsPublisher for handling integration events with RabbitMQ

---
+++ apps/mba-ddd-venda-ingresso/src/domain-events/integration-events.publisher.ts
@@ -0,0 +1,19 @@
+import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
+import { Job } from 'bull';
+import { IIntegrationEvent } from '../@core/common/domain/integration-event';
+import { Process, Processor } from '@nestjs/bull';
+
+@Processor('integration-events')
+export class IntegrationEventsPublisher {
+  constructor(private amqpConnection: AmqpConnection) {}
+
+  @Process()
+  async handle(job: Job<IIntegrationEvent>) {
+    await this.amqpConnection.publish(
+      'amq.direct',
+      job.data.event_name,
+      job.data,
+    );
+    return {};
+  }
+}
