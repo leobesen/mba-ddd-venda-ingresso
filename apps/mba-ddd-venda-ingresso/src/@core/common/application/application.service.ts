91a03de1dc65421c4d0a20040b2e606bb03e3185
Leonardo Besen <leobesen@hotmail.com>
2026-04-01 16:49:57 +0000

    feat: enhance finish method to publish integration events for aggregate roots

--- apps/mba-ddd-venda-ingresso/src/@core/common/application/application.service.ts
+++ apps/mba-ddd-venda-ingresso/src/@core/common/application/application.service.ts
@@ -15,6 +15,9 @@ export class ApplicationService {
       await this.domainEventManager.publish(aggregateRoot);
     }
     await this.uow.commit();
+    for (const aggregateRoot of aggregateRoots) {
+      await this.domainEventManager.publishForIntegrationEvent(aggregateRoot);
+    }
   }
 
   fail() {}
